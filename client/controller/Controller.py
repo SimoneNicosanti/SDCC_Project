076760c52d90db0c465a4f907ee4c28b11e0624f
andreaxdf <andreadefilippis00@gmail.com>
2023-08-21 19:10:55 +0200

    RabbitMQ configuration

--- client/controller/Controller.py
+++ client/controller/Controller.py
@@ -1,27 +1,30 @@
 from engineering import RabbitSingleton
 from engineering.Message import Message, Method, MessageEncoder
-import json
+import json, pika
 
-def getFile(fileName : str) -> None :
+def sendRequestForFile(requestType, fileName):
     channel = RabbitSingleton.getRabbitChannel()
-    channel.queue_declare(queue = 'storage_queue')
+    channel.queue_declare(queue = 'storage_queue', durable=True)
 
-    message : Message = Message(Method.GET, "file_prova")
+    message : Message = Message(requestType, fileName)
     channel.basic_publish(
         exchange = '', 
         routing_key = 'storage_queue', 
-        body = json.dumps(obj = message, cls = MessageEncoder)
+        body = json.dumps(obj = message, cls = MessageEncoder),
+        properties = pika.BasicProperties(delivery_mode = pika.spec.PERSISTENT_DELIVERY_MODE)
     )
-    print("Sent Message")
-    pass
+    print(f"[*] {requestType} request sent for file '{fileName}'")
+
+def getFile(fileName : str) -> None :
+    sendRequestForFile(Method.GET, "file_di_prova.txt")
 
 
 def putFile(fileName : str) -> None :
-    pass
+    sendRequestForFile(Method.PUT, "file_di_prova.txt")
 
 
 def deleteFile(fileName : str) -> None:
-    pass
+    sendRequestForFile(Method.DEL, "file_di_prova.txt")
 
 
 def login(username : str, passwd : str, email : str) -> bool :
--- edge/peer/peer.go
+++ edge/peer/peer.go
@@ -1,6 +1,7 @@
 package peer
 
 import (
+	"bytes"
 	"edge/utils"
 	"errors"
 	"log"
@@ -9,6 +10,8 @@ import (
 	"net/rpc"
 	"strings"
 	"time"
+
+	amqp "github.com/rabbitmq/amqp091-go"
 )
 
 /*
@@ -55,6 +58,61 @@ func ActAsPeer() {
 	defer registryClientPtr.Close()
 
 	var forever chan struct{}
+	<-forever
+}
+
+//host='rabbit_mq', port = '5672'
+func consumeMessages() {
+	conn, err := amqp.Dial("amqp://guest:guest@rabbit_mq:5672/")
+	utils.ExitOnError("Failed to connect to RabbitMQ", err)
+	defer conn.Close()
+
+	ch, err := conn.Channel()
+	utils.ExitOnError("Failed to open a channel", err)
+	defer ch.Close()
+
+	q, err := ch.QueueDeclare(
+		"storage_queue", // name
+		true,            // durable
+		false,           // delete when unused
+		false,           // exclusive
+		false,           // no-wait
+		nil,             // arguments
+	)
+	utils.ExitOnError("Failed to declare a queue", err)
+
+	err = ch.Qos(
+		1,     // prefetch count
+		0,     // prefetch size
+		false, // global
+	)
+	utils.ExitOnError("Failed to set QoS", err)
+
+	msgs, err := ch.Consume(
+		q.Name, // queue
+		"",     // consumer
+		false,  // auto-ack
+		false,  // exclusive
+		false,  // no-local
+		false,  // no-wait
+		nil,    // args
+	)
+	utils.ExitOnError("Failed to register as a consumer", err)
+
+	var forever chan struct{}
+
+	go func() {
+		for d := range msgs {
+			log.Printf("Received a message: %s", d.Body)
+			dotCount := bytes.Count(d.Body, []byte("."))
+			t := time.Duration(dotCount)
+			time.Sleep(t * time.Second)
+			log.Printf("Done")
+			d.Ack(false)
+		}
+	}()
+
+	log.Printf("[*] Waiting for messages. To exit press CTRL+C")
 	<-forever
 }
 
