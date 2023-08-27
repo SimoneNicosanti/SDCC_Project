3f2546d6437277db9a1840d3cab809b6432254e0
EdoMan000 <manenti000@gmail.com>
2023-08-27 19:47:04 +0200

    continuo interazione tra edge per ricerca file nella rete p2p

--- docker/proto_compile.sh
+++ docker/proto_compile.sh
@@ -1,2 +1,3 @@
-docker compose exec edge protoc -I=/proto --go_out=/src/proto --go-grpc_out=/src/proto /proto/File.proto
-docker compose exec client python -m grpc_tools.protoc -I/proto --python_out=/src/proto --pyi_out=/src/proto --grpc_python_out=/src/proto /proto/file_transfer/File.proto /proto/login/Login.proto
+docker compose exec edge protoc -I=/proto --go_out=/src/proto --go-grpc_out=/src/proto /proto/ClientFileTransfer.proto
+docker compose exec edge protoc -I=/proto --go_out=/src/proto --go-grpc_out=/src/proto /proto/EdgeFileTransfer.proto
+docker compose exec client python -m grpc_tools.protoc -I/proto --python_out=/src/proto --pyi_out=/src/proto --grpc_python_out=/src/proto /proto/file_transfer/ClientFileTransfer.proto /proto/login/Login.proto
