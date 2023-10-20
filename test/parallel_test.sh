ed52ebd9fdf206bdea7173db3cc3028bb0f081e3
EdoMan000 <manenti000@gmail.com>
2023-10-20 19:00:20 +0200

    running other tests

--- test/parallel_test.sh
+++ test/parallel_test.sh
@@ -46,34 +46,64 @@ then
             docker exec -t -i sdcc_project-client-1 python3 Main.py -u test -p test -o upload -f 250mb.txt -e $2 -w Parallel.csv &
         done
 
+        ## To wait termination of previous requests
         sleep 15
     done
 fi
 
 
-# Test Upload
-# if [ $1 = "down_all" ]
-# then
-#     echo "TEST DOWNLOAD CACHE"
-#     for i in $( seq 1 $MAX_RUN )
-#     do
-#         # CACHABLE
+## Test Download
+if [ $1 = "down_all" ]
+then
 
-#         docker exec -t -i sdcc_project-client-1 python3 Main.py -u test -p test -o upload -f 5mb.txt -e $2  -w Parallel.csv &
-#         echo
-#         docker exec -t -i sdcc_project-client-1 python3 Main.py -u test -p test -o upload -f 10mb.txt -e $2 -w Parallel.csv &
-#         echo 
-#         docker exec -t -i sdcc_project-client-1 python3 Main.py -u test -p test -o upload -f 20mb.txt -e $2 -w Parallel.csv &
-#         echo
-#         docker exec -t -i sdcc_project-client-1 python3 Main.py -u test -p test -o upload -f 30mb.txt -e $2 -w Parallel.csv &
-#         echo
-#         # NO CACHABLE
-#         docker exec -t -i sdcc_project-client-1 python3 Main.py -u test -p test -o upload -f 100mb.txt -e $2 -w Parallel.csv &
-#         echo
-#         docker exec -t -i sdcc_project-client-1 python3 Main.py -u test -p test -o upload -f 250mb.txt -e $2 -w Parallel.csv &
-#         echo
-#     done
-# fi
+    echo "UPLOADING ALL FILES"
+    docker exec -t -i sdcc_project-client-1 python3 Main.py -u test -p test -o upload -f 5mb.txt -e $2 -w NO
+    echo
+    docker exec -t -i sdcc_project-client-1 python3 Main.py -u test -p test -o upload -f 10mb.txt -e $2 -w NO
+    echo 
+    docker exec -t -i sdcc_project-client-1 python3 Main.py -u test -p test -o upload -f 20mb.txt -e $2 -w NO
+    echo
+    docker exec -t -i sdcc_project-client-1 python3 Main.py -u test -p test -o upload -f 30mb.txt -e $2 -w NO
+    echo
+    docker exec -t -i sdcc_project-client-1 python3 Main.py -u test -p test -o upload -f 100mb.txt -e $2 -w NO
+    echo
+    docker exec -t -i sdcc_project-client-1 python3 Main.py -u test -p test -o upload -f 250mb.txt -e $2 -w NO
+    echo
+
+    ## Remove from edge caches
+    for i in $( seq 1 $2 )
+    do
+        docker exec -t  sdcc_project-edge-$i /bin/sh rm /files/*
+    done
+
+    echo "TEST DOWNLOAD"
+    for i in $( seq 1 $MAX_RUN )
+    do
+        for j in $(seq 1 $CACHABLE_REQ_NUM)
+        do
+            # CACHABLE
+            docker exec -t -i sdcc_project-client-1 python3 Main.py -u test -p test -o download -f 5mb_test.txt -e $2  -w Parallel.csv &
+            echo
+            docker exec -t -i sdcc_project-client-1 python3 Main.py -u test -p test -o download -f 10mb_test.txt -e $2 -w Parallel.csv &
+            echo 
+            docker exec -t -i sdcc_project-client-1 python3 Main.py -u test -p test -o download -f 20mb_test.txt -e $2 -w Parallel.csv &
+            echo
+            docker exec -t -i sdcc_project-client-1 python3 Main.py -u test -p test -o download -f 30mb_test.txt -e $2 -w Parallel.csv &
+            echo
+        done
+
+        # NO CACHABLE
+        for j in $( seq 1 $NO_CACHABLE_REQ_NUM)
+        do
+            docker exec -t -i sdcc_project-client-1 python3 Main.py -u test -p test -o download -f 100mb_test.txt -e $2 -w Parallel.csv &
+            echo
+            docker exec -t -i sdcc_project-client-1 python3 Main.py -u test -p test -o download -f 250mb_test.txt -e $2 -w Parallel.csv &
+            echo
+        done
+
+        sleep 15
+    done
+fi
 
 
 
